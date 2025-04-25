fc0932f4c303a668a1942b8ca1d05581fd5bcddd
LeoHolmer <leonardoholmer1@gmail.com>
2025-04-24 23:42:29 -0300

    first commit

---
+++ src/test/java/com/example/pdyc2025_junin_individual/Pdyc2025JuninIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdyc2025_junin_individual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pdyc2025JuninIndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
